eb26023ab2a478787e030e37e904265686229ed1
Chris Schoutrop <c.e.m.schoutrop@tue.nl>
2022-12-04 09:24:54 +0100

    small cleanup

--- 4/4.cpp
+++ 4/4.cpp
@@ -2,15 +2,15 @@
 
 int main()
 {
-    std::vector<std::string> rucksacks = read_input("4_input.txt");
-    // std::vector<std::string> rucksacks = read_input("4_test.txt");
+    std::vector<std::string> input = read_input("4_input.txt");
+    // std::vector<std::string> input = read_input("4_test.txt");
 
     i64 sum;
     // part 1
     sum = 0;
-    for (auto s : rucksacks)
+    for (auto s : input)
     {
-        std::vector<i64> assignments, r1;
+        std::vector<i64> assignments;
         assignments = split_input(s);
         if (check_full_overlap(assignments))
         {
@@ -20,9 +20,9 @@ int main()
     std::cout << "sum: " << sum << std::endl;
 
     sum = 0;
-    for (auto s : rucksacks)
+    for (auto s : input)
     {
-        std::vector<i64> assignments, r1;
+        std::vector<i64> assignments;
         assignments = split_input(s);
         if (check_some_overlap(assignments))
         {
